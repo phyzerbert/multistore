80be525d4710c8b111ecdde27231d353da6d1e85
phyzerbert <xian1017@outlook.com>
2019-07-09 08:00:09 +0800

    update dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,8 @@ use App\Models\Purchase;
 use App\Models\Sale;
 use App\Models\Order;
 
+use Carbon\Carbon;
+
 class HomeController extends Controller
 {
     /**
@@ -25,12 +27,43 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
         config(['site.page' => 'home']);
         $total_purchases = Order::where('orderable_type', Purchase::class)->sum('subtotal');
         $total_sales = Order::where('orderable_type', Sale::class)->sum('subtotal');
 
-        return view('home', compact('total_purchases', 'total_sales'));
+        $period = '';
+        if($request->has('period') && $request->get('period') != ""){   
+            $period = $request->get('period');
+            $from = substr($period, 0, 10);
+            $to = substr($period, 14, 10);
+            // $mod = $mod->whereBetween('timestamp', [$from, $to]);
+        }
+
+        if(isset($from) && isset($to)){
+            $chart_start = Carbon::createFromFormat('Y-m-d', $from);
+            $chart_end = Carbon::createFromFormat('Y-m-d', $to);
+        }else{
+            $chart_start = Carbon::now()->startOfMonth();
+            $chart_end = Carbon::now()->endOfMonth();
+        }
+        
+        $key_array = $purchases = $sales = $purchase_array = $sale_array = array();
+
+        for ($dt=$chart_start; $dt < $chart_end; $dt->addDay()) {
+            $key = $dt->format('Y-m-d');
+            $key1 = $dt->format('M/d');
+            array_push($key_array, $key1);
+            $purchases = Purchase::whereDate('timestamp', $key)->pluck('id')->toArray();
+            $sales = Sale::whereDate('timestamp', $key)->pluck('id')->toArray();
+            $daily_purchase = Order::whereIn('orderable_id', $purchases)->sum('subtotal');
+            $daily_sale = Order::whereIn('orderable_id', $sales)->sum('subtotal');
+            array_push($purchase_array, $daily_purchase);
+            array_push($sale_array, $daily_sale);
+        }
+
+        return view('home', compact('total_purchases', 'total_sales', 'key_array', 'purchase_array', 'sale_array', 'period'));
     }
+
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,13 +18,47 @@
         @endphp
         <div class="br-pagebody">
             <div class="br-section-wrapper">
-                
+                <div class="row"></div>
+                <div class="row">
+                    <div class="card card-body">
+                        <canvas id="line_chart" style="height:400px;"></canvas>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 @endsection
 
 @section('script')
+<script src="{{asset('master/lib/chart.js/Chart.js')}}"></script>
+<script>
+
+    var lineData = {
+        labels: {!! json_encode($key_array) !!},
+        datasets: [
+            {
+                label: "Purchase",
+                backgroundColor: 'rgba(52,152,219, 0.6)',
+                borderColor: 'rgba(52,152,219, 1)',
+                pointBorderColor: "#fff",
+                data: {!! json_encode($purchase_array) !!},
+            },{
+                label: "Sale",
+                backgroundColor: 'rgba(213,217,219, 0.6)',
+                borderColor: 'rgba(213,217,219, 1)',
+                pointBorderColor: "#fff",
+                data: {!! json_encode($sale_array) !!},
+            }
+        ]
+    };
+    var lineOptions = {
+        responsive: true,
+        maintainAspectRatio: false
+    };
+    var ctx = document.getElementById("line_chart").getContext("2d");
+    new Chart(ctx, {type: 'line', data: lineData, options:lineOptions});
+
+</script>
 <script>
     $(document).ready(function () {
 
